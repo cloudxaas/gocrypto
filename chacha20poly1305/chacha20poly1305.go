65610c59d4481750b11e65e1c6581f438372cc3f
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2023-04-27 04:57:46 -0700

    Update chacha20poly1305.go

--- chacha20poly1305/chacha20poly1305.go
+++ chacha20poly1305/chacha20poly1305.go
@@ -4,37 +4,37 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"io"
-        "golang.org/x/crypto/chacha20poly1305"
 
+	"golang.org/x/crypto/chacha20poly1305"
 )
 
 // ChaCha20Poly1305Encrypt encrypts the given plaintext with the given key and nonce
 // using the ChaCha20-Poly1305 AEAD cipher. The ciphertext is returned.
 // The key length must be 32 bytes, and the nonce length must be 12 bytes.
 func ChaCha20Poly1305Encrypt(key, nonce, plaintext []byte) ([]byte, error) {
-	block, err := cipher.NewChaCha20Poly1305(key)
+	aead, err := chacha20poly1305.NewX(key)
 	if err != nil {
 		return nil, err
 	}
 
 	// Generate a random buffer for the ciphertext
-	ciphertext := make([]byte, len(plaintext)+block.Overhead())
+	ciphertext := make([]byte, len(plaintext)+aead.Overhead())
 
 	// Seal the plaintext with the given nonce
-	return block.Seal(ciphertext[:0], nonce, plaintext, nil), nil
+	return aead.Seal(ciphertext[:0], nonce, plaintext, nil), nil
 }
 
 // ChaCha20Poly1305Decrypt decrypts the given ciphertext with the given key and nonce
 // using the ChaCha20-Poly1305 AEAD cipher. The plaintext is returned.
 // The key length must be 32 bytes, and the nonce length must be 12 bytes.
 func ChaCha20Poly1305Decrypt(key, nonce, ciphertext []byte) ([]byte, error) {
-	block, err := cipher.NewChaCha20Poly1305(key)
+	aead, err := chacha20poly1305.NewX(key)
 	if err != nil {
 		return nil, err
 	}
 
 	// Decrypt the ciphertext with the given nonce
-	plaintext, err := block.Open(nil, nonce, ciphertext, nil)
+	plaintext, err := aead.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
 		return nil, err
 	}
